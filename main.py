9ba747c209d76a8cf6e300e81f1e13290249f005
Robert Granda <robert@MacBook-Pro-de-Robert.local>
2023-09-16 09:31:34 -0500

    MI primer scraper

---
+++ main.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.us.despegar.com/flights/UIO/AGP?from=SB&di=1-0&reSearch=true")
+flights = driver.find_elements(By.CLASS_NAME, "cluster-container")
+for f in flights:
+    airline_name = f.find_element(by=By.TAG_NAME, value="img").accessible_name
+    departure = f.find_element(by=By.CLASS_NAME, value="cluster-part-0").text
+    arrival = f.find_element(by=By.CLASS_NAME, value="cluster-part-1").text
+    days = f.find_element(by=By.CLASS_NAME, value="quantity-days").text
+    price = f.find_element(by=By.CLASS_NAME, value="price").text
+
+    print(airline_name)
+    print(departure)
+    print(arrival)
+    print(days)
+    print(price)
+    print('=' * 40)
+
+
+
+
+
+
+
+# elem.clear()
+# elem.send_keys("pycon")
+# elem.send_keys(Keys.RETURN)
+driver.close()
